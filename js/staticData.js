1d550e1a764fa4130f1e0d08a0de855994b07bdd
Yuting Hsu <b98705030@gmail.com>
2016-04-28 22:42:57 -0400

    move static data to staticData.js

---
+++ js/staticData.js
@@ -0,0 +1,20 @@
+var events = {  
+    "1939": {"places": [[41.57, 12.30],[9.145000, 40.489673],[51.165691, 10.451526]], "eventname": "WORLD WAR II", "img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"},
+    "1940": {"places": [[35.861660, 104.195397],[36.204824, 138.252924],[47.516231, 14.550072],[51.165691, 10.451526],[41.871940, 12.56738],[44.980733, 20.169453],[56.263920, 9.501785],[60.472024, 8.468946] ], "eventname": "WORLD WAR II","img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"},
+    "1941": {"places": [[23.416203, 25.66283],[42.733883, 25.48583],[47.162494, 19.503304],[51.220643, 51.363519],[55.755826, 37.6173],[21.344507, -157.974891],[12.879721, 121.774017],[14.058324, 108.277199],[1.352083, 103.819836]], "eventname": "WORLD WAR II","img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"}, 
+    "1942": {"places": [[45.813398, 15.964656],[28.207217, -177.373493],[48.708048, 44.513303],[-9.645710, 160.156194],[28.033886, 1.659626],[33.886917, 9.537499],[31.869706, 115.747628],[45.943161, 24.96676]], "eventname": "WORLD WAR II"},
+    "1943": {"places": [[33.886917, 9.537499],[37.599994, 14.015356],[51.709196, 36.156224],[42.017239, 14.859782],[26.142036, -81.79481]], "eventname": "WORLD WAR II","img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"},
+    "1944": {"places": [[41.449596, 12.619725],[37.981835, -80.423567],[48.879870, 0.171253],[53.709807, 27.953389],[48.856614, 2.352222],[43.710173, 7.261953],[12.879721, 121.774017],[50.503887, 4.469936]], "eventname": "WORLD WAR II","img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"},
+    "1945": {"places": [[50.573280, 7.238553],[52.520007, 13.404954],[45.815011, 15.981919],[47.516231, 14.550072],[41.871940, 12.56738],[26.212401, 127.680932],[34.385203, 132.455293],[19.445411, -99.089734]], "eventname": "WORLD WAR II","img_src":"http://vignette3.wikia.nocookie.net/marveldatabase/images/9/9d/World_War_II_001.jpg/revision/latest/scale-to-width-down/200?cb=20090825012758"},
+    "1960": {"places":[[61.524010, 105.318756], [37.090240, -95.712891]], "eventname": "The Space Race", "img_src":"http://x.annihil.us/u/prod/marvel/i/mg/6/20/519b7424af23c/portrait_incredible.jpg"},
+    "1971": {"places": [[40.7891, -73.1350]], "eventname":"Drug Abuse","img_src": "http://i.annihil.us/u/prod/marvel/i/mg/e/40/4bc33acb2b5e0/portrait_incredible.jpg"},
+    "1982": {"places":[[61.524010, 105.318756]], "eventname":"Fall of the Soviet Union"},
+    "2001": {"places":[[40.783060, -73.971249],[38.881621, -77.090981]], "eventname":"9/11", "img_src":"http://cdn.bleedingcool.net/wp-content/uploads/2013/06/image80.jpg?f6a06b"}
+};
+
+var avengers = ["Captain America", "Tony Stark", "Thor", "Dr. Bruce Banner", "Natasha Romanoff", "Hawkeye"];
+var shield = ["Nick Fury", "Phil Coulson", "Maria Hill", "Peggy Carter"];
+var xmen = ["Professor Charles Xavier", "Erik Lehnsherr", "Raven Darkholme", "Jean Grey", "Dr. Henry 'Hank' McCoy", "Logan", "Wade Wilson"];
+var fantastic4 = ["Mr. Fantastic", "Invisible Woman", "Human Torch", "Thing"];
+var spider = ["Peter Parker"];
+var guardians = ["Peter Quill", "Gamora", "Rocket Raccoon", "Drax the Destroyer", "Groot"];
